924f1bb0cd8d35f6c4d4ab3ea96a2fab1a37d3ba
chenglla <1225883052@qq.com>
2020-07-04 22:26:38 +0800

    河北收益修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import qs from 'qs'
 
 // 创建axios实例
 const service = axios.create({
-  baseURL: 'https://www.zytb.top/show/cee/', // api的base_url
+  baseURL: 'http://www.kgai.tech/cee/', // api的base_url
   // baseURL: 'http://58.119.112.15:11010/', // api的base_url
   // baseURL: 'http://127.0.0.1:8000/', // api的base_url
   timeout: 50000 // 请求超时时间
